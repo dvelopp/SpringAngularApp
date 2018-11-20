eed15ed74dcf87525e4997aacc18b47e5b73e013
dvelopp <dvelopp@gmail.com>
2018-11-20 22:35:06 +0200

    Moving to docker. Upgrading to Spring 2.

--- src/main/java/springAngularApp/system/configuration/SpringJpaConfig.java
+++ src/main/java/springAngularApp/system/configuration/SpringJpaConfig.java
@@ -2,7 +2,6 @@ package springAngularApp.system.configuration;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.jpa.HibernateEntityManagerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
